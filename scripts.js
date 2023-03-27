ee03fbd61ce04da44b550f3a53d8223f8177d46d
paulocesar89 <pc_schmitz@outlook.com>
2023-03-27 19:50:46 -0300

    inicio de contagem dos ponteiros de minutos e horas

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
 setInterval(() =>{
 
     const secondPointer = document.querySelector('.secondPointer');
+    const minutePointer = document.querySelector('.minutePointer');
+    const hourPointer = document.querySelector('.hourPointer');
 
     horario = new Date();
-    second = horario.getSeconds() * 6;
+    second = horario.getSeconds() * 6; //multiplicar os segundos por 6 pois 1 segundo é igual a 6º no relogio (360º / 60 segundos = 6º)
+    minute = horario.getMinutes() * 6;
+    hour = horario.getHours() * 30;
 
 
     secondPointer.style.transform = `translate(-30%, 20%) rotate(${second}deg)`;
+    minutePointer.style.transform = `translate(-30%, 20%) rotate(${minute}deg)`;
+    hourPointer.style.transform = `translate(-30%, 45%) rotate(${hour}deg)`;
     
 }, 1000);
